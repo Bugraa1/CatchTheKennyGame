4bcf2bd816d97d9e84eaebec75a3fbde476c0c4f
Yusuf Buğra Yıldırım <143610683+Bugraa1@users.noreply.github.com>
2025-03-03 13:45:33 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Yusuf Bugra on 3.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
